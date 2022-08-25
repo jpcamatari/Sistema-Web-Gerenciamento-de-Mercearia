be9d9f9ee5601eede15cdd61ca645d27aeaad9f6
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-08-24 22:24:21 -0300

    Inicialização da Controller

--- DAO.py
+++ DAO.py
@@ -1,4 +1,3 @@
-from turtle import st
 from model import *
 
 class DaoCategoria:
--- controller.py
+++ controller.py
@@ -1,3 +1,19 @@
-from DAO import DaoVendas
+from DAO import DaoCategoria, DaoVendas
+from model import *
 
-DaoVendas.ler()
+
+class ControllerCategoria:
+    def cadastraCategoria(self, novaCategoria):
+        existe = False
+        x = DaoCategoria.ler()
+        for i in x:
+            if i.categoria == novaCategoria:
+                existe = True 
+        if not existe:
+            DaoCategoria.salvar(novaCategoria)
+            print('Categoria Cadastrada com Sucesso!')
+        else:
+            print('A Categoria que deseja cadastrar ja existe')
+
+v = DaoVendas()
+v.ler()
