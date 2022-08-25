caed247b3aa897693ba38a2688699c571b542243
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-08-24 23:44:44 -0300

    Atualização da controller Categoria

--- controller.py
+++ controller.py
@@ -15,5 +15,39 @@ class ControllerCategoria:
         else:
             print('A Categoria que deseja cadastrar ja existe')
 
-v = DaoVendas()
-v.ler()
+    def removerCategoria(self, categoriaRemover):
+        x = DaoCategoria.ler()
+        cat = list(filter(lambda x: x.categoria == categoriaRemover, x))
+
+        if len(cat) <= 0:
+            print('A categoria que deseja remover não existe')
+        else:
+            for i in range(len(x)):
+                if x[i].categoria == categoriaRemover:
+                    del x[i]
+                    break
+            print('Categoria removida com sucesso')
+
+            with open('categoria.txt', 'w') as arq:
+                for i in x:
+                    arq.writelines(i.categoria)
+                    arq.writelines('\n')
+
+    def alterarCategoria(self, categoriaAlterar, categoriaAlterada):
+        x = DaoCategoria.ler()
+        cat = list(filter(lambda x: x.categoria == categoriaAlterar, x))
+
+        if len(cat) > 0:
+            cat1 = list(filter(lambda x: x.categoria == categoriaAlterada, x))
+            if len(cat1) == 0:
+                x = list(map(lambda x: Categoria(categoriaAlterada) if (x.categoria == categoriaAlterar) else(x), x))
+                print('A Alteração foi efetuada com sucesso.')
+            else:
+                print('A categoria para qual deseja alterar ja existe')
+        else:
+            print('A categoria que deseja alterar não existe')
+        
+        with open('categoria.txt', 'w') as arq:
+            for i in x:
+                arq.writelines(i.categoria)
+                arq.writelines('\n')
