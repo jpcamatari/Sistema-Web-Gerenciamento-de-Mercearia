80418a49cad31a9c1e22427c525247719ce50709
João Paulo Antonio Camatari <jpcamatari@hotmail.com>
2022-09-16 00:13:00 -0300

    Resolução de problema de importação da DAO

--- DAO.py
+++ DAO.py
@@ -1,140 +1,133 @@
-from model import *
+from Models import *
 
 class DaoCategoria:
     @classmethod
     def salvar(cls, categoria):
-        with open("Categoria.txt", 'a') as arq:
+        with open('categoria.txt', 'a') as arq:
             arq.writelines(categoria)
             arq.writelines('\n')
 
     @classmethod
     def ler(cls):
-        with open("Categoria.txt", 'r') as arq:
+        with open('categoria.txt', 'r') as arq:
             cls.categoria = arq.readlines()
-        
+
         cls.categoria = list(map(lambda x: x.replace('\n', ''), cls.categoria))
-       
+
         cat = []
         for i in cls.categoria:
             cat.append(Categoria(i))
 
         return cat
 
-class DaoVendas:
+class DaoVenda:
     @classmethod
-    def salvar(cls, vendas: Vendas):
-        with open("Vendas.txt", 'a') as arq:
-            arq.writelines(vendas.produto.nome + "|" + vendas.produto.preco + "|" + vendas.produto.categoria + "|" + vendas.funcionario + "|" + 
-            vendas.cliente + "|" + str(vendas.quantidadeVendida) + "|" + vendas.data)
+    def salvar(cls, venda: Venda):
+        with open('venda.txt', 'a') as arq:
+            arq.writelines(venda.itensVendido.nome + "|" + venda.itensVendido.preco + "|" +
+                           venda.itensVendido.categoria + "|" + venda.vendededor + "|" +
+                           venda.comprador + "|" + str(venda.quantidadeVendida) + "|" + venda.data)
             arq.writelines('\n')
 
     @classmethod
     def ler(cls):
-        with open("Vendas.txt", 'r') as arq:
-            cls.vendas = arq.readlines()
-        
-        cls.vendas = list(map(lambda x: x.replace('\n', ''), cls.vendas))
-        cls.vendas = list(map(lambda x: x.split('|'), cls.vendas))
-        ven = []
-        for i in cls.vendas:
-            ven.append(Vendas(Produto(i[0], i[1], i[2]), i[3], i[4], i[5], i[6]))
-        return ven
+        with open('venda.txt', 'r') as arq:
+            cls.venda = arq.readlines()
+
+        cls.venda = list(map(lambda x: x.replace('\n', ''), cls.venda))
+        cls.venda = list(map(lambda x: x.split('|'), cls.venda))
+        vend = []
+        for i in cls.venda:
+            vend.append(Venda(Produtos(i[0], i[1], i[2]), i[3], i[4], i[5], i[6]))
+        return vend
+
 
 class DaoEstoque:
     @classmethod
-    def salvar(cls, produto: Produto, quantidade):
-        with open("Estoque.txt", 'a') as arq:
-            arq.writelines(produto.nome + '|' + produto.preco + '|' + produto.categoria + '|' + str(quantidade))
+    def salvar(cls, produto: Produtos, quantidade):
+        with open('estoque.txt', 'a') as arq:
+            arq.writelines(produto.nome + "|" + produto.preco + "|" +
+                           produto.categoria + "|" + str(quantidade))
             arq.writelines('\n')
 
     @classmethod
     def ler(cls):
-        with open("Estoque.txt", 'r') as arq:
+        with open('estoque.txt', 'r') as arq:
             cls.estoque = arq.readlines()
-        
+
         cls.estoque = list(map(lambda x: x.replace('\n', ''), cls.estoque))
         cls.estoque = list(map(lambda x: x.split('|'), cls.estoque))
         est = []
         if len(cls.estoque) > 0:
             for i in cls.estoque:
-                est.append(Estoque(Produto(i[0], i[1], i[2]), int(i[3])))
+                est.append(Estoque(Produtos(i[0], i[1], i[2]), int(i[3])))
 
         return est
 
 class DaoFornecedor:
     @classmethod
-    def salvar(cls, fornecedor: Fornecedor):
-        with open("Fornecedor.txt", 'a') as arq:
-            arq.writelines(fornecedor.empresa + '|' + fornecedor.categoria)
+    def salvar(cls, fornecedor : Fornecedor):
+        with open('fornecedores.txt', 'a') as arq:
+            arq.writelines(fornecedor.nome + "|" + fornecedor.cnpj + "|" + fornecedor.telefone
+                           + "|" + fornecedor.categoria)
             arq.writelines('\n')
 
     @classmethod
     def ler(cls):
-        with open("Fornecedor.txt", 'r') as arq:
-            cls.fornecedor = arq.readlines()
+        with open('fornecedores.txt', 'r') as arq:
+            cls.fornecedores = arq.readlines()
 
-        cls.fornecedor = list(map(lambda x: x.replace('\n', ''), cls.fornecedor))
-        cls.fornecedor = list(map(lambda x: x.split('|'), cls.fornecedor))
+        cls.fornecedores = list(map(lambda x: x.replace('\n', ''), cls.fornecedores))
+        cls.fornecedores = list(map(lambda x: x.split('|'), cls.fornecedores))
         forn = []
-        for i in cls.fornecedor:
-            forn.append(Fornecedor(i[0], i[1]))
+        for i in cls.fornecedores:
+            forn.append(Fornecedor(i[0], i[1], i[2], i[3]))
 
         return forn
 
-class DaoCliente:
+
+
+class DaoPessoa:
     @classmethod
-    def salvar(cls, cliente: Cliente):
-        with open("Cliente.txt", 'a') as arq:
-            arq.writelines(cliente.nome_cliente)
+    def salvar(cls, pessoas: Pessoa):
+        with open('clientes.txt', 'a') as arq:
+            arq.writelines(pessoas.nome + "|" + pessoas.telefone + "|" + pessoas.cpf
+                           + "|" + pessoas.email + "|" + pessoas.endereco)
             arq.writelines('\n')
 
     @classmethod
     def ler(cls):
-        with open("Cliente.txt", 'r') as arq:
-            cls.cliente = arq.readlines()
-        
-        cls.cliente = list(map(lambda x: x.replace('\n', ''), cls.cliente))
-        cli = []
-        for i in cls.cliente:
-            cli.append(Cliente(i))
+        with open('clientes.txt', 'r') as arq:
+            cls.clientes = arq.readlines()
 
-        return cli
+        cls.clientes = list(map(lambda x: x.replace('\n', ''), cls.clientes))
+        cls.clientes = list(map(lambda x: x.split('|'), cls.clientes))
 
+        clientes = []
+        for i in cls.clientes:
+            clientes.append(Pessoa(i[0], i[1], i[2], i[3], i[4]))
 
-class DaoProduto:
-    @classmethod
-    def salvar(cls, produto: Produto):
-        with open("Produto.txt", 'a') as arq:
-            arq.writelines(produto.nome)
-            arq.writelines('\n')
+        return clientes
 
-    @classmethod
-    def ler(cls):
-        with open("Produto.txt", 'r') as arq:
-            cls.produto = arq.readlines()
-        
-        cls.produto = list(map(lambda x: x.replace('\n', ''), cls.produto))
-        pro = []
-        for i in cls.produto:
-            pro.append(Produto(i))
-
-        return pro
 
 class DaoFuncionario:
     @classmethod
     def salvar(cls, funcionario: Funcionario):
-        with open("Funcionario.txt", 'a') as arq:
-            arq.writelines(funcionario.nome_fun)
+        with open('funcionarios.txt', 'a') as arq:
+            arq.writelines(funcionario.clt + "|" + funcionario.nome + "|" + funcionario.telefone
+                           + "|" + funcionario.cpf + "|" + funcionario.email + "|" + funcionario.endereco)
             arq.writelines('\n')
 
     @classmethod
     def ler(cls):
-        with open("Funcionario.txt", 'r') as arq:
-            cls.funcionario = arq.readlines()
-        
-        cls.funcionario = list(map(lambda x: x.replace('\n', ''), cls.funcionario))
-        fun = []
-        for i in cls.funcionario:
-            fun.append(Funcionario(i))
+        with open('funcionarios.txt', 'r') as arq:
+            cls.funcionarios = arq.readlines()
 
-        return fun
+        cls.funcionarios = list(map(lambda x: x.replace('\n', ''), cls.funcionarios))
+        cls.funcionarios = list(map(lambda x: x.split('|'), cls.funcionarios))
+
+        funcionario = []
+        for i in cls.funcionarios:
+            funcionario.append(Funcionario(i[0], i[1], i[2], i[3], i[4], i[5]))
+
+        return funcionario
--- controller.py
+++ controller.py
@@ -1,20 +1,19 @@
-from tkinter import N
-from model import Categoria, Estoque, Produto, Fornecedor, Cliente, Funcionario, Vendas
-from DAO import DaoCategoria, DaoVendas, DaoEstoque, DaoFornecedor, DaoCliente, DaoFuncionario
+from Models import Categoria, Estoque, Produtos, Fornecedor, Pessoa, Funcionario, Venda
+from DAO import DaoCategoria, DaoVenda, DaoEstoque, DaoFornecedor, DaoPessoa, DaoFuncionario
 from datetime import datetime
-
 class ControllerCategoria:
     def cadastraCategoria(self, novaCategoria):
         existe = False
         x = DaoCategoria.ler()
         for i in x:
             if i.categoria == novaCategoria:
-                existe = True 
+                existe = True
+
         if not existe:
             DaoCategoria.salvar(novaCategoria)
-            print('Categoria Cadastrada com Sucesso!')
+            print('Categoria cadastrada com sucesso')
         else:
-            print('A Categoria que deseja cadastrar ja existe')
+            print('A categoria que deseja cadastrar já existe')
 
     def removerCategoria(self, categoriaRemover):
         x = DaoCategoria.ler()
@@ -28,39 +27,59 @@ class ControllerCategoria:
                     del x[i]
                     break
             print('Categoria removida com sucesso')
-            #TODO: COLOCAR SEM CATEGORIA NO ESTOQUE
             with open('categoria.txt', 'w') as arq:
                 for i in x:
                     arq.writelines(i.categoria)
                     arq.writelines('\n')
 
+        estoque = DaoEstoque.ler()
+
+        estoque = list(map(lambda x: Estoque(Produtos(x.produto.nome, x.produto.preco, "Sem categoria"), x.quantidade) if(x.produto.categoria == categoriaRemover) else(x), estoque))
+        with open('estoque.txt', 'w') as arq:
+            for i in estoque:
+                arq.writelines(i.produto.nome + "|" + i.produto.preco + "|" + i.produto.categoria + "|" + str(i.quantidade))
+                arq.writelines("\n")
+
     def alterarCategoria(self, categoriaAlterar, categoriaAlterada):
         x = DaoCategoria.ler()
+
         cat = list(filter(lambda x: x.categoria == categoriaAlterar, x))
 
         if len(cat) > 0:
             cat1 = list(filter(lambda x: x.categoria == categoriaAlterada, x))
             if len(cat1) == 0:
-                x = list(map(lambda x: Categoria(categoriaAlterada) if (x.categoria == categoriaAlterar) else(x), x))
-                print('A Alteração foi efetuada com sucesso.')
-                #TODO: ALTERAR A CATEGORIA DO ESTOQUE           
+                x = list(map(lambda x: Categoria(categoriaAlterada) if(x.categoria == categoriaAlterar) else(x) ,x))
+                print('A altereção foi efetuada com sucesso')
+
+                estoque = DaoEstoque.ler()
+
+                estoque = list(map(
+                    lambda x: Estoque(Produtos(x.produto.nome, x.produto.preco, categoriaAlterada), x.quantidade) if (
+                                x.produto.categoria == categoriaAlterar) else (x), estoque))
+                with open('estoque.txt', 'w') as arq:
+                    for i in estoque:
+                        arq.writelines(i.produto.nome + "|" + i.produto.preco + "|" + i.produto.categoria + "|" + str(
+                            i.quantidade))
+                        arq.writelines("\n")
+
             else:
-                print('A categoria para qual deseja alterar ja existe')
+                print("A categoria para qual deseja alterar já existe")
+
         else:
             print('A categoria que deseja alterar não existe')
-        
+
         with open('categoria.txt', 'w') as arq:
             for i in x:
                 arq.writelines(i.categoria)
                 arq.writelines('\n')
 
     def mostrarCategoria(self):
-        categoria = DaoCategoria.ler()
-        if len(categoria) == 0:
-            print('Categoria Vazia')
+        categorias = DaoCategoria.ler()
+        if len(categorias) == 0:
+            print('Categoria vazia!')
         else:
-            for i in categoria:
-                print(f'Categorias: {i.categoria}')
+            for i in categorias:
+                print(f'Categoria: {i.categoria}')
 
 class ControllerEstoque:
     def cadastrarProduto(self, nome, preco, categoria, quantidade):
@@ -71,63 +90,62 @@ class ControllerEstoque:
 
         if len(h) > 0:
             if len(est) == 0:
-                produto = Produto(nome, preco, categoria)
+                produto = Produtos(nome, preco, categoria)
                 DaoEstoque.salvar(produto, quantidade)
-                print('Produto Cadastrado com sucesso.')
+                print('Produco cadastrado com sucesso')
             else:
-                print('Produto já existe em Estoque')
+                print('Produto já existe em estoque')
         else:
-            print('Categoria Inexistente')
+            print('Categoria inexistente')
 
     def removerProduto(self, nome):
         x = DaoEstoque.ler()
         est = list(filter(lambda x: x.produto.nome == nome, x))
-
         if len(est) > 0:
             for i in range(len(x)):
                 if x[i].produto.nome == nome:
                     del x[i]
-                    print('Produto Removido com Sucesso.')
                     break
+            print('Produto foi removido com sucesso')
         else:
             print('O produto que deseja remover não existe')
 
-        with open('Estoque.txt', 'w') as arq:
+        with open('estoque.txt', 'w') as arq:
             for i in x:
-                arq.writelines(i.produto.nome + '|' + i.produto.preco + '|' + i.produto.categoria + '|' + str(i.quantidade))
+                arq.writelines(i.produto.nome + "|" + i.produto.preco + "|" +
+                               i.produto.categoria + "|" + str(i.quantidade))
                 arq.writelines('\n')
-   
+
     def alterarProduto(self, nomeAlterar, novoNome, novoPreco, novaCategoria, novaQuantidade):
         x = DaoEstoque.ler()
         y = DaoCategoria.ler()
         h = list(filter(lambda x: x.categoria == novaCategoria, y))
-
         if len(h) > 0:
             est = list(filter(lambda x: x.produto.nome == nomeAlterar, x))
             if len(est) > 0:
                 est = list(filter(lambda x: x.produto.nome == novoNome, x))
                 if len(est) == 0:
-                    x = list(map(lambda x: Estoque(Produto(novoNome, novoPreco, novaCategoria), novaQuantidade) if(x.produto.nome == nomeAlterar) else(x), x))
-                    print('Produto alterado com sucesso')               
+                    x = list(map(lambda x: Estoque(Produtos(novoNome, novoPreco, novaCategoria), novaQuantidade) if(x.produto.nome == nomeAlterar) else(x), x))
+                    print('Produto alterado com sucesso')
                 else:
-                    print('Produto ja cadastrado.')
+                    print('Produto já cadastro')
             else:
-                print('O produto que deseja alterar não existe.')
+                print('O produto que deseja alterar não existe')
 
-            with open('Estoque.txt', 'w') as arq:
+            with open('estoque.txt', 'w') as arq:
                 for i in x:
-                    arq.writelines(i.produto.nome + '|' + i.produto.preco + '|' + i.produto.categoria + '|' + str(i.quantidade))
+                    arq.writelines(i.produto.nome + "|" + i.produto.preco + "|" +
+                                   i.produto.categoria + "|" + str(i.quantidade))
                     arq.writelines('\n')
-                
         else:
             print('A categoria informada não existe')
 
     def mostrarEstoque(self):
         estoque = DaoEstoque.ler()
         if len(estoque) == 0:
-            print('Estoque Vazio')
+            print('Estoque vazio')
         else:
-            print('*****Produtos*****')
+            print("==========Produtos==========")
             for i in estoque:
                 print(f"Nome: {i.produto.nome}\n"
                       f"Preco: {i.produto.preco}\n"
@@ -143,7 +161,6 @@ class ControllerVenda:
         temp = []
         existe = False
         quantidade = False
-
         for i in x:
             if existe == False:
                 if i.produto.nome == nomeProduto:
@@ -151,52 +168,57 @@ class ControllerVenda:
                     if i.quantidade >= quantidadeVendida:
                         quantidade = True
                         i.quantidade = int(i.quantidade) - int(quantidadeVendida)
-                        vendido = Vendas(Produto(i.produto.nome, i.produto.preco, i.produto.categoria), vendedor, comprador, quantidadeVendida)
-                        valorCompra = int(quantidadeVendida)*int(i.produto.preco)
 
-                        DaoVendas.salvar(vendido)
+                        vendido = Venda(Produtos(i.produto.nome, i.produto.preco, i.produto.categoria), vendedor, comprador, quantidadeVendida)
 
-            temp.append(Estoque(Produto(i.produto.nome, i.produto.preco, i.produto.categoria), i.quantidade))              
-            arq = open('estoque.txt', 'w')
-            arq.write("")
+                        valorCompra = int(quantidadeVendida) * int(i.produto.preco)
 
-            for i in temp:
-                with open('estoque.txt', 'a') as arq:
-                    arq.writelines(i.produto.nome + '|' + i.produto.preco + '|' + i.produto.categoria + '|' + str(i.quantidade))
-                    arq.writelines('\n')
+                        DaoVenda.salvar(vendido)
 
-            if existe == False:
-                print('O produto não existe')
-                return None
-            elif not quantidade:
-                print('A qauntidade vendida não contem em estoque')
-            else:
-                return valorCompra
+            temp.append(Estoque(Produtos(i.produto.nome, i.produto.preco, i.produto.categoria), i.quantidade))
+
+        arq = open('estoque.txt', 'w')
+        arq.write("")
+
+
+        for i in temp:
+            with open('estoque.txt', 'a') as arq:
+                arq.writelines(i.produto.nome + "|" + i.produto.preco + "|" + i.produto.categoria + "|" + str(i.quantidade))
+                arq.writelines('\n')
+
+        if existe == False:
+            print('O produto nao exite')
+            return None
+        elif not quantidade:
+            print('A quantidade vendida não contem em estoque')
+            return None
+        else:
+            print('Venda realizada com sucesso')
+            return valorCompra
 
     def relatorioProdutos(self):
-        vendas = DaoVendas.ler()
+        vendas = DaoVenda.ler()
         produtos = []
         for i in vendas:
-            nome = i.itensVendidos.nome
+            nome = i.itensVendido.nome
             quantidade = i.quantidadeVendida
             tamanho = list(filter(lambda x: x['produto'] == nome, produtos))
             if len(tamanho)>0:
-                produtos = list(map(lambda x: {'Produto': nome, 'quantidade': int(x['quantidade']) + int(quantidade)}
-                 if (x['produto'] == nome) else(x), produtos))
+                produtos = list(map(lambda x: {'produto': nome, 'quantidade': int(x['quantidade']) + int(quantidade)}
+                if (x['produto'] == nome) else(x), produtos))
             else:
-                produtos.append({'Produto': nome, 'quantidade': int(quantidade)})
+                produtos.append({'produto': nome, 'quantidade': int(quantidade)})
 
-            ordenado = sorted(produtos, key=lambda k: k['quantidade'], reverse=True)
+        ordenado = sorted(produtos, key=lambda k: k['quantidade'], reverse=True)
 
-            print('Esses são os produtos mais vendidos:')
-            a = 1
-            for i in ordenado:
-                print(f"======Produto [{a}]======")
-                print(f"Produto: {i['produtos']}\n"
-                f"Quantidade: {i['quantidade']}\n")
-                   
-                a += 1
+        print('Esses são os produtos mais vendidos')
+        a = 1
+        for i in ordenado:
+            print(f"==========Produto [{a}]==========")
+            print(f"Produto: {i['produto']}\n"
+                  f"Quantidade: {i['quantidade']}\n")
+            a += 1
 
 a = ControllerVenda()
-a.cadastrarVenda('maca', 'caio', 'pedro', '5')
+a.relatorioProdutos()
 
